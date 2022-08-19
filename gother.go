5c5f63a41bc9f680ff0da8a16829c7d6504bd354
Tell396 <vova21473@gmail.com>
2022-08-19 21:12:52 +1000

    Story #11. Error handling was added.

--- gother.go
+++ gother.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -10,6 +11,7 @@ import (
 func main() {
 	//colorReset := "\033[0m"
 
+	// Setting colors
 	colorRed := "\033[31m"
 	colorGreen := "\033[32m"
 	colorYellow := "\033[33m"
@@ -23,15 +25,14 @@ func main() {
 	fmt.Print(string(colorPurple), "Type a city >>> ")
 	fmt.Scanln(&city)
 
+	// Print info
 	data, err := query(city)
 	if err != nil {
 		fmt.Println(err.Error(), http.StatusInternalServerError)
 		return
-	}
-
-	if city != "" {
+	} else {
 		fmt.Println()
-		fmt.Println(string(colorBlue), data.Name + ", " + data.Sys.Country)
+		fmt.Println(string(colorBlue), data.Name+", "+data.Sys.Country)
 
 		fmt.Println(string(colorYellow), "/- ====================== -/")
 		fmt.Println()
@@ -41,32 +42,30 @@ func main() {
 
 		fmt.Println()
 
-    fmt.Println(string(colorGreen), "Temperature:", string(colorRed), data.Main.Celsius, "°C")
-    fmt.Println(string(colorGreen), "Fells like:", string(colorRed), data.Main.FellsLike)
+		fmt.Println(string(colorGreen), "Temperature:", string(colorRed), data.Main.Celsius, "°C")
+		fmt.Println(string(colorGreen), "Fells like:", string(colorRed), data.Main.FellsLike)
 
-    fmt.Println(string(colorGreen), "Visibility:", string(colorRed), data.Visibility)
-    fmt.Println(string(colorGreen), "Clouds:", string(colorRed), data.Clouds.All, "%")
-		
-    fmt.Println()
+		fmt.Println(string(colorGreen), "Visibility:", string(colorRed), data.Visibility)
+		fmt.Println(string(colorGreen), "Clouds:", string(colorRed), data.Clouds.All, "%")
+
+		fmt.Println()
 		fmt.Println(string(colorYellow), "/- ====================== -/")
 		fmt.Println()
-	} else {
-		fmt.Println(string(colorRed), "Nope")
 	}
 }
 
 // Extract data from API
 type weatherData struct {
-	Name    string `json:"name"`
-  Visibility float64 `json:"visibility"`
-	
-  Weather []struct {
+	Name       string  `json:"name"`
+	Visibility float64 `json:"visibility"`
+
+	Weather []struct {
 		Main        string `json:"main"`
 		Description string `json:"description"`
 	}
-  Clouds struct {
-    All float64 `json:"all"`
-  }
+	Clouds struct {
+		All float64 `json:"all"`
+	}
 	Sys struct {
 		Country string `json:"country"`
 	}
@@ -97,23 +96,24 @@ func loadApiConfig(filename string) (apiConfigData, error) {
 	return c, nil
 }
 
+// Get data from API
 func query(city string) (weatherData, error) {
 	var d weatherData
 
 	apiConfig, err := loadApiConfig(".apiConfig.json")
 	if err != nil {
-		return weatherData{}, err
+		return weatherData{}, errors.New("Failed to get API key.")
 	}
 
 	resp, err := http.Get("http://api.openweathermap.org/data/2.5/weather?APPID=" + apiConfig.OpenWeatherMapApiKey + "&q=" + city + "&units=metric")
 	if err != nil {
-		return weatherData{}, err
+		return weatherData{}, errors.New("Failed response from server.")
 	}
 
 	defer resp.Body.Close()
 
 	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
-		return weatherData{}, err
+		return weatherData{}, errors.New("Error.")
 	}
 
 	return d, nil
